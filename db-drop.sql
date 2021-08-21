5be99d82bfda173e37047784229dcb3fdda9c016
Benny Li <bli@upgrade.com>
2021-08-21 10:54:48 -0700

    Add the generated db-script.

---
+++ db-drop.sql
@@ -0,0 +1,6 @@
+alter table BookingDate drop foreign key FK_BookingDate_Booking_Id
+drop table if exists Booking
+drop table if exists BookingDate
+alter table BookingDate drop foreign key FK_BookingDate_Booking_Id
+drop table if exists Booking
+drop table if exists BookingDate
